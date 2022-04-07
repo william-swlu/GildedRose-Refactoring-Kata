babd3028fd4c11607d2569f141b54b763d5d4eb7
William Lu <wi3015415@gmail.com>
2022-04-07 23:01:21 +0800

    sellIn 拉到前面

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -26,30 +26,28 @@ class Shop {
     this.items.forEach((item) => {
       if (item.name == 'Sulfuras, Hand of Ragnaros') return
 
+      item.sellIn = item.sellIn - 1;
+
       if (item.name == 'Aged Brie') {
         this.increaseQuality(item)
-      } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-        this.increaseQuality(item)
-        if (item.sellIn < 11) {
+        if (item.sellIn < 0) {
           this.increaseQuality(item)
         }
-        if (item.sellIn < 6) {
+      } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+        this.increaseQuality(item)
+        if (item.sellIn < 10) {
           this.increaseQuality(item)
+        }
+        if (item.sellIn < 5) {
+          this.increaseQuality(item)
+        }
+        if (item.sellIn < 0) {
+          item.quality = 0
         }
       } else {
         this.decreaseQuality(item)
-      }
-      item.sellIn = item.sellIn - 1;
-      if (item.sellIn < 0) {
-        if (item.name == 'Aged Brie') {
-          this.increaseQuality(item)
-        } else {
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            item.quality = item.quality - item.quality;
-          }
-          else {
-            this.decreaseQuality(item)
-          }
+        if (item.sellIn < 0) {
+          this.decreaseQuality(item)
         }
       }
     })
