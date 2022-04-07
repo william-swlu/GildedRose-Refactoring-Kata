eba2d8910525ade593762f7e73ace5e28e50a088
William Lu <wi3015415@gmail.com>
2022-04-08 00:13:27 +0800

    抽class

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -16,6 +16,51 @@ class Item {
       this.quality = this.quality - 1;
     }
   }
+
+  update() {
+    this.sellIn = this.sellIn - 1;
+  }
+}
+
+class AgedBrie extends Item {
+  update() {
+    super.update();
+    this.increaseQuality()
+    if (this.sellIn < 0) {
+      this.increaseQuality()
+    }
+  }
+}
+
+class BackstagePasses extends Item {
+  update() {
+    super.update();
+    this.increaseQuality()
+    if (this.sellIn < 10) {
+      this.increaseQuality()
+    }
+    if (this.sellIn < 5) {
+      this.increaseQuality()
+    }
+    if (this.sellIn < 0) {
+      this.quality = 0
+    }
+  }
+}
+
+class Sulfuras extends Item {
+  update() {
+  }
+}
+
+class NormalItem extends Item {
+  update() {
+    super.update();
+    this.decreaseQuality()
+    if (this.sellIn < 0) {
+      this.decreaseQuality()
+    }
+  }
 }
 
 class Shop {
@@ -36,31 +81,37 @@ class Shop {
 
   updateQuality() {
     this.items.forEach((item) => {
-      if (item.name == 'Sulfuras, Hand of Ragnaros') return
+      if (item.name == 'Sulfuras, Hand of Ragnaros') {
+        item.update()
+        // return
+      }
 
-      item.sellIn = item.sellIn - 1;
+      // item.sellIn = item.sellIn - 1;
 
       if (item.name == 'Aged Brie') {
-        item.increaseQuality()
-        if (item.sellIn < 0) {
-          item.increaseQuality()
-        }
+        // item.increaseQuality()
+        // if (item.sellIn < 0) {
+        //   item.increaseQuality()
+        // }
+        item.update()
       } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-        item.increaseQuality()
-        if (item.sellIn < 10) {
-          item.increaseQuality()
-        }
-        if (item.sellIn < 5) {
-          item.increaseQuality()
-        }
-        if (item.sellIn < 0) {
-          item.quality = 0
-        }
+        // item.increaseQuality()
+        // if (item.sellIn < 10) {
+        //   item.increaseQuality()
+        // }
+        // if (item.sellIn < 5) {
+        //   item.increaseQuality()
+        // }
+        // if (item.sellIn < 0) {
+        //   item.quality = 0
+        // }
+        item.update()
       } else {
-        item.decreaseQuality()
-        if (item.sellIn < 0) {
-          item.decreaseQuality()
-        }
+        // item.decreaseQuality()
+        // if (item.sellIn < 0) {
+        //   item.decreaseQuality()
+        // }
+        item.update()
       }
     })
 
@@ -69,5 +120,9 @@ class Shop {
 }
 module.exports = {
   Item,
-  Shop
+  Shop,
+  AgedBrie,
+  BackstagePasses,
+  Sulfuras,
+  NormalItem
 }
